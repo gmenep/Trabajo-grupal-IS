23b3337500c1337229f44b63abbe571a5303290b
Guillén Menéndez Pellitero <gmenep01@estudiantes.unileon.es>
2026-05-07 13:48:05 +0200

    encryptador bcrypt

---
+++ src/modelo/PasswordLogica.py
@@ -0,0 +1,18 @@
+import bcrypt
+
+
+class PasswordService:
+
+    @staticmethod
+    def hash_password(password: str) -> str:
+        password_bytes = password.encode("utf-8")
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password_bytes, salt)
+        return hashed_password.decode("utf-8")
+
+    @staticmethod
+    def verify_password(password: str, hashed_password: str) -> bool:
+        password_bytes = password.encode("utf-8")
+        hashed_password_bytes = hashed_password.encode("utf-8")
+
+        return bcrypt.checkpw(password_bytes, hashed_password_bytes)
